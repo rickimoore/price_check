2e4063a7e5d643ab5627375f7343b227858f913c
Mavrik <mrricki.m.usmc@gmail.com>
2023-12-03 23:56:39 +0100

    feat : add formatting and upgrades

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
+use rodio::{Decoder, OutputStream, Sink};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use rodio::{OutputStream, Sink, Decoder};
 use std::fs::File;
 use std::io::BufReader;
 use tokio::time::{self, Duration};
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[clap(short, long, default_value= "BTC")]
+    asset: String,
+    #[clap(short, long, default_value= "USDC")]
+    currency: String,
+    #[clap(short, long, default_value= "100.0")]
+    diff: f64,
+}
 
 #[derive(Deserialize, Serialize, Debug)]
 struct ResponseData {
@@ -35,25 +46,20 @@ fn play_jingle(path: &str) {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() {
     let mut initial_rate: f64;
     let mut lower_low: f64;
-    let asset: String = std::env::args().nth(1).unwrap_or_else(|| {
-        println!("Please provide valid Coinbase asset id");
-        std::process::exit(-1);
-    }).to_uppercase();
-    let currency: String = std::env::args().nth(2).unwrap_or_else(|| {
-        println!("Please provide valid conversion rate");
-        std::process::exit(-1);
-    }).to_uppercase();
-    let difference: f64 = std::env::args().nth(3).unwrap_or_else(|| {
-        println!("Please provide valid alert range for increase or decrease in exchange rates.");
-        std::process::exit(-1);
-    }).parse().unwrap();
+
+    let cli = Args::parse();
+
+
+    let asset: String = cli.asset.to_uppercase();
+    let currency: String = cli.currency.to_uppercase();
+    let difference: f64 = cli.diff;
 
     println!("[ {} ] asset accepted...", asset);
     println!("[ {} ] currency rate accepted...", currency);
-
+    println!("Monitoring for price change of +- [ {} ]", difference);
 
     println!("Contacting coinbase api...");
     match fetch_rate(&asset, &currency).await {
@@ -61,12 +67,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             println!("Successful api response...");
             initial_rate = rate;
             lower_low = rate;
-            println!("Setting initial exchange rate {} to USD: {}", asset, initial_rate);
-        },
+            println!(
+                "Setting initial exchange rate {} to USD: {}",
+                asset, initial_rate
+            );
+        }
         Err(e) => {
             eprintln!("Error fetching rate: {}", e);
             std::process::exit(-1);
-        },
+        }
     }
 
     let mut interval = time::interval(Duration::from_secs(10));
@@ -79,24 +88,33 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 if (rate - initial_rate) > difference {
                     initial_rate = rate;
                     lower_low = rate;
-                    println!("Setting new initial exchange rate {} to USD: {}", asset, initial_rate);
+                    println!(
+                        "Setting new initial exchange rate {} to USD: {}",
+                        asset, initial_rate
+                    );
                     play_jingle("../big_pimpin.mp3");
                 } else if (lower_low - rate) > difference {
                     lower_low = rate;
-                    println!("Woah going the wrong way, setting new lower low at {}", lower_low);
+                    println!(
+                        "Woah going the wrong way, setting new lower low at {}",
+                        lower_low
+                    );
                     play_jingle("../nooo.mp3");
                 }
-            },
+            }
             Err(e) => {
                 eprintln!("Error fetching rate: {}", e);
                 std::process::exit(-1);
-            },
+            }
         }
     }
 }
 
 async fn fetch_rate(asset: &str, currency: &str) -> Result<f64, Box<dyn std::error::Error>> {
-    let url = format!("https://api.coinbase.com/v2/exchange-rates?currency={}", asset);
+    let url = format!(
+        "https://api.coinbase.com/v2/exchange-rates?currency={}",
+        asset
+    );
 
     let response = reqwest::get(&url).await?;
 
@@ -105,7 +123,7 @@ async fn fetch_rate(asset: &str, currency: &str) -> Result<f64, Box<dyn std::err
         let parsed_data: Response = serde_json::from_str(&body)?;
 
         if let Some(rate) = parsed_data.data.rates.get(currency) {
-            Ok(rate.parse().unwrap())
+            Ok(rate.parse()?)
         } else {
             Err("Currency not found in coinbase api rates".into())
         }
